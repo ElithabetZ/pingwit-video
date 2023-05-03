02791b7e27c082451990a18c18d99abacea9e8b8
ElithabetZ <elizyshchuk@gmail.com>
2023-05-03 12:38:01 +0200

    Primitive types

---
+++ src/main/java/org/example/lec_1/TypesExample.java
@@ -0,0 +1,18 @@
+package org.example.lec_1;
+
+public class TypesExample {
+    public static void main (String[] args) {
+        byte myByte = 127;
+        int myInt = 128;
+
+        char myChar = 1000;
+        char mySymbolChar = 'K';
+        char myCharUTF = '\u33F4';
+        System.out.println(myChar);
+        System.out.println(mySymbolChar);
+        System.out.println(myCharUTF);
+
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+    }
+}
